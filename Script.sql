8f30323f884b0784e43d53156bedae8feeb2d4fc
Izarbe02 <zarbix02@gmail.com>
2025-03-03 18:47:41 +0100

    Inserts organizadores y eventos

--- Script.sql
+++ Script.sql
@@ -155,14 +155,14 @@ SET IDENTITY_INSERT Usuarios OFF;
 
 SET IDENTITY_INSERT Organizador ON;
 INSERT INTO Organizador (ID, nombre, ubicacion, descripcion, enlace, email, contrasenia, telefono, idRol) VALUES 
-(1, 'ZaragozaConecta', 'Zaragoza, España', 'Organizador de eventos comunitarios', 'http://zaragoconecta.es', 'info@zaragoconecta.es', 'claveorg1', '976123456', 2),
-(2, 'Colectivo Feminista', 'Zaragoza, España', 'Organización feminista autogestionada', 'http://feminismo.es', 'contacto@feminismo.es', 'claveorg2', '976789123', 2),
-(3, 'Asamblea de Vecinos', 'Zaragoza, España', 'Colectivo vecinal que organiza actividades culturales y de barrio.', 'http://vecinoszgz.com', 'vecinos@zgz.com', 'claveorg3', '976456789', 2),
-(4, 'Red de Apoyo Mutuo', 'Zaragoza, España', 'Grupo que organiza encuentros y charlas sobre autogestión.', 'http://apoyomutuo.org', 'apoyomutuo@zgz.com', 'claveorg4', '976654321', 2),
-(5, 'CSO La Fábrica de Chocolate', 'Zaragoza, España', 'Centro social autogestionado que organiza múltiples actividades culturales, políticas y sociales.', 'http://lafabricadechocolate.es', 'contacto@lafabricadechocolate.es', 'claveorg5', '976333111', 2),
-(6, 'Arrebato', 'Zaragoza, España', 'Sala de conciertos autogestionada con programación cultural y artística independiente.', 'http://arrebato.es', 'contacto@arrebato.es', 'claveorg6', '976222888', 2),
-(7, 'Kike Mur', 'Zaragoza, España', 'Centro social autogestionado con actividades comunitarias, culturales y sociales.', 'http://kikemur.es', 'contacto@kikemur.es', 'claveorg7', '976111777', 2),
-(8, 'Centro Social Librería La Pantera Rossa', 'Zaragoza, España', 'Centro social librería. Pensamiento crítico+autoorganización', 'http://kikemur.es', 'contacto@pantera.es', 'claveorg8', '976111777', 2);
+(1, 'ZaragozaConecta', 'Zaragoza, España', 'Organizador de eventos comunitarios', 'https://i.pinimg.com/736x/48/1e/ea/481eea34012312eb2c70a22a7b811ff0.jpg', 'info@zaragoconecta.es', 'claveorg1', '976123456', 2),
+(2, 'Colectivo Carlista', 'Zaragoza, España', 'Organización Carlista autogestionada', 'https://i.pinimg.com/736x/20/72/0d/20720da22007b96922129c7fd7bd6848.jpg', 'contacto@feminismo.es', 'claveorg2', '976789123', 2),
+(3, 'Asamblea de Vecinos', 'Zaragoza, España', 'Colectivo vecinal que organiza actividades culturales y de barrio.', 'https://i.pinimg.com/736x/f1/0a/38/f10a3833dad60e72d76240763108c899.jpg', 'vecinos@zgz.com', 'claveorg3', '976456789', 2),
+(4, 'Red de Apoyo Mutuo', 'Zaragoza, España', 'Grupo que organiza encuentros y charlas sobre autogestión.', 'https://i.pinimg.com/736x/5e/66/3d/5e663d55ee7de7adaa8931a692ff5d23.jpg', 'apoyomutuo@zgz.com', 'claveorg4', '976654321', 2),
+(5, 'CSO La Fábrica de Chocolate', 'Zaragoza, España', 'Centro social autogestionado que organiza múltiples actividades culturales, políticas y sociales.', 'https://www.aragondigital.es/media/aragondigital/images/2024/11/13/2024111311403021287.jpg', 'contacto@lafabricadechocolate.es', 'claveorg5', '976333111', 2),
+(6, 'Arrebato', 'Zaragoza, España', 'Sala de conciertos autogestionada con programación cultural y artística independiente.', 'https://cdn.aragonmusical.com/wp-content/uploads/2016/09/Viva-Belgrado-660x330.jpg', 'contacto@arrebato.es', 'claveorg6', '976222888', 2),
+(7, 'Kike Mur', 'Zaragoza, España', 'Centro social autogestionado con actividades comunitarias, culturales y sociales.', 'https://pbs.twimg.com/media/DbJg9bOXkAA-Qgi.jpg', 'contacto@kikemur.es', 'claveorg7', '976111777', 2),
+(8, 'Centro Social Librería La Pantera Rossa', 'Zaragoza, España', 'Centro social librería. Pensamiento crítico+autoorganización', 'https://www.lapanterarossa.net/sites/default/files/inline-images/colabora.jpg', 'contacto@pantera.es', 'claveorg8', '976111777', 2);
 SET IDENTITY_INSERT Organizador OFF;
 
 
@@ -213,31 +213,31 @@ SET DATEFORMAT YMD;
 SET IDENTITY_INSERT Eventos ON;
 INSERT INTO Eventos (ID, nombre, descripcion, ubicacion, fecha_inicio, fecha_fin, enlace, idOrganizador)
 VALUES 
-(1, 'La primera vez que bailamos', 'Exposición de collages de la artista Laura Miqueo.', 'La Pantera Rossa', '2025-02-20 10:00', '2025-03-09 18:00', 'http://lapanterarossa.es/expo', 5),
-(2, 'Por unos parques y jardines dignos', 'Concentración por el mantenimiento adecuado de espacios verdes.', 'Plaza Ontonar', '2025-02-28 17:00', '2025-02-28 18:00', 'http://zaragoconecta.es/parques', 1),
-(3, 'Alimentación sana y agricultura ecológica', 'Charla sobre hábitos saludables y sostenibilidad.', 'La Pantera Rossa', '2025-02-28 18:00', '2025-02-28 20:00', 'http://lapanterarossa.es/alimentacion', 5),
-(4, 'Cine por la paz: Parar las guerras', 'Proyección de documentales sobre conflictos bélicos y pacifismo.', 'Filmoteca de Zaragoza', '2025-02-28 18:00', '2025-02-28 20:00', 'http://filmotecazaragoza.es/cinepaz', 1),
-(5, 'Cadena humana por los seis de Zaragoza', 'Movilización en solidaridad con los seis de Zaragoza.', 'Delegación del Gobierno', '2025-02-28 18:30', '2025-02-28 19:30', 'http://zaragoconecta.es/solidaridad6', 1),
-(6, 'Concierto: Sal del coche + Muelles', 'Noche de música independiente.', 'A.VV. Arrebato', '2025-02-28 20:00', '2025-02-28 22:00', 'http://arrebato.es/concierto', 6),
-(7, 'Facharnaval', 'Vermú carnavalero en apoyo a las radios libres.', 'CSO La Fábrica de Chocolate', '2025-03-01 12:00', '2025-03-01 18:00', 'http://lafabricadechocolate.es/vermucarnaval', 5),
-(8, 'Presentación de "Mauro"', 'Novela sobre una de las mayores evasiones carcelarias de Europa.', 'La Pantera Rossa', '2025-03-01 12:00', '2025-03-01 14:00', 'http://lapanterarossa.es/mauro', 5),
-(9, 'Taller: Desmontando al facha de tu clase + Fiesta', 'Debate y análisis crítico del auge de la extrema derecha.', 'Centro Cultural Socialista La Comuna', '2025-03-01 17:00', '2025-03-01 00:00', 'http://lacomuna.es/taller', 1),
-(10, 'Cine por la paz 2', 'Segunda jornada de proyección de documentales.', 'Filmoteca de Zaragoza', '2025-03-01 18:00', '2025-03-01 20:00', 'http://filmotecazaragoza.es/cinepaz2', 1),
-(11, 'Concierto Apoyo XXII Marcha Zuera', 'Concierto solidario en apoyo a la marcha anual a la cárcel de Zuera.', 'A.VV. Arrebato', '2025-03-01 20:00', '2025-03-01 22:00', 'http://arrebato.es/marchazuera', 6),
-(12, 'Torneo de ajedrez "Por la paz en Palestina"', 'Competencia amistosa con enfoque en la justicia social.', 'Casa Palestina de Aragón', '2025-03-02 16:00', '2025-03-02 18:00', 'http://casapalestina.es/ajedrez', 1),
-(13, 'Taller de lectura', 'Espacio de lectura y análisis del libro "La llamada" de Leila Guerriero.', 'La Pantera Rossa', '2025-03-03 19:00', '2025-03-03 21:00', 'http://lapanterarossa.es/tallerlectura', 5),
-(14, 'Charla: Mujeres, disidencias y revolución', 'Conversación sobre feminismo y luchas sociales.', 'Unizar, Aula 4.5', '2025-03-06 19:00', '2025-03-06 20:30', 'http://unizar.es/charlafeminismo', 2),
-(15, 'Huelga y manifestación estudiantil 7M', 'Marcha por los derechos estudiantiles.', 'Plaza San Francisco', '2025-03-07 12:00', '2025-03-07 14:00', 'http://zaragoconecta.es/estudiantes7m', 1),
+(1, 'La primera vez que bailamos', 'Exposición de collages de la artista Laura Miqueo.', 'La Pantera Rossa', '2025-02-20 10:00', '2025-03-09 18:00', 'https://zgz.convoca.la/media/1a7cec4dc2aa72605c5ea53e74f80440.jpg', 5),
+(2, 'Por unos parques y jardines dignos', 'Concentración por el mantenimiento adecuado de espacios verdes.', 'Plaza Ontonar', '2025-02-28 17:00', '2025-02-28 18:00', 'https://jardiarte.es/wp-content/uploads/2020/01/DSC_2892-1024x576.jpg', 1),
+(3, 'Alimentación sana y agricultura ecológica', 'Charla sobre hábitos saludables y sostenibilidad.', 'La Pantera Rossa', '2025-02-28 18:00', '2025-02-28 20:00', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtPN-55jloKOoRxP5bvF3xCB-RXy8RZMuKvA&s', 5),
+(4, 'Cine por la paz: Parar las guerras', 'Proyección de documentales sobre conflictos bélicos y pacifismo.', 'Filmoteca de Zaragoza', '2025-02-28 18:00', '2025-02-28 20:00', 'https://zgz.convoca.la/media/a5231b88663f0b7ff0a267e33493b3ef.jpg', 1),
+(5, 'Cadena humana por los seis de Zaragoza', 'Movilización en solidaridad con los seis de Zaragoza.', 'Delegación del Gobierno', '2025-02-28 18:30', '2025-02-28 19:30', 'https://imagenes.heraldo.es/files/image_990_556/uploads/imagenes/2024/02/23/manifestacion-en-apoyo-a-los-6-de-zaragoza-1.jpeg', 1),
+(6, 'Concierto: Sal del coche + Muelles', 'Noche de música independiente.', 'A.VV. Arrebato', '2025-02-28 20:00', '2025-02-28 22:00', 'https://f4.bcbits.com/img/a3238194177_65', 6),
+(7, 'Facharnaval', 'Vermú carnavalero en apoyo a las radios libres.', 'CSO La Fábrica de Chocolate', '2025-03-01 12:00', '2025-03-01 18:00', 'https://zgz.convoca.la/media/74f90f9792111d83e0b5f526a8be6db4.jpg', 5),
+(8, 'Presentación de "Mauro"', 'Novela sobre una de las mayores evasiones carcelarias de Europa.', 'La Pantera Rossa', '2025-03-01 12:00', '2025-03-01 14:00', 'https://www.kulturklik.euskadi.eus/contenidos/creacion/2025020508060060/es_def/images/I.png', 5),
+(9, 'Taller: Desmontando al facha de tu clase + Fiesta', 'Debate y análisis crítico del auge de la extrema derecha.', 'Centro Cultural Socialista La Comuna', '2025-03-01 17:00', '2025-03-01 00:00', 'https://estaticos-cdn.prensaiberica.es/clip/aa9939a3-704c-4c04-a4be-5e7b0514b390_alta-libre-aspect-ratio_default_0.jpg', 1),
+(10, 'Cine por la paz 2', 'Segunda jornada de proyección de documentales.', 'Filmoteca de Zaragoza', '2025-03-01 18:00', '2025-03-01 20:00', 'https://www.enjoyzaragoza.es/wp-content/uploads/2022/11/Captura-de-Pantalla-2023-09-18-a-las-10.12.32.png', 1),
+(11, 'Concierto Apoyo XXII Marcha Zuera', 'Concierto solidario en apoyo a la marcha anual a la cárcel de Zuera.', 'A.VV. Arrebato', '2025-03-01 20:00', '2025-03-01 22:00', 'https://zgz.convoca.la/media/11711629221f29e8c4786cb75b0f8e9c.jpg', 6),
+(12, 'Torneo de ajedrez "Por la paz en Palestina"', 'Competencia amistosa con enfoque en la justicia social.', 'Casa Palestina de Aragón', '2025-03-02 16:00', '2025-03-02 18:00', 'https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/AquivaRubinstein/phpVvOvC8.png', 1),
+(13, 'Taller de lectura', 'Espacio de lectura y análisis del libro "La llamada" de Leila Guerriero.', 'La Pantera Rossa', '2025-03-03 19:00', '2025-03-03 21:00', 'https://www.lapanterarossa.net/sites/default/files/leilaWeb.png', 5),
+(14, 'Charla: disidencias y revolución', 'Conversación sobre  luchas sociales.', 'Unizar, Aula 4.5', '2025-03-06 19:00', '2025-03-06 20:30', 'https://i.pinimg.com/736x/aa/99/11/aa9911cf9c637071e5e05b6a0377c5ec.jpg', 2),
+(15, 'Huelga y manifestación estudiantil 7M', 'Marcha por los derechos estudiantiles.', 'Plaza San Francisco', '2025-03-07 12:00', '2025-03-07 14:00', 'https://www.izquierdadiario.es/IMG/jpg/dsc00743.jpg', 1),
 (16, 'Concierto: Disciplina Limitar + Guiñote de Qontaqto', 'Noche de punk y rock alternativo.', 'A.VV. Arrebato', '2025-03-14 20:00', '2025-03-14 22:00', 'http://arrebato.es/disciplina', 6),
 (17, 'Presentación del libro "Vértebras"', 'La poeta Pilar Roig Ferreruela presenta su nuevo poemario.', 'La Pantera Rossa', '2025-03-15 12:00', '2025-03-15 14:00', 'http://lapanterarossa.es/vertebras', 5),
 (18, 'Asesoría laboral', 'Sesión de asesoramiento gratuito sobre derechos laborales.', 'AAVV Venecia/Torrero', '2025-03-26 18:00', '2025-03-26 20:00', 'http://venecia-torrero.es/asesorialaboral', 1),
-(19, 'Concierto solidario con la PAH', 'Evento musical para recaudar fondos.', 'CSP Amapola', '2025-03-29 19:00', '2025-03-29 21:00', 'http://cspamapola.es/conciertopah', 1),
-(20, 'XXII Marcha a la macrocárcel de Zuera', 'Movilización anual contra la represión y el sistema carcelario.', 'Macrocárcel de Zuera', '2025-03-30 11:00', '2025-03-30 18:00', 'http://zaragoconecta.es/marchazuera', 1),
-(21, 'Tatu Circus Zgz', 'Festival de tatuajes solidario con eventos culturales.', 'CSO Kike Mur', '2025-05-02 10:00', '2025-05-04 22:00', 'http://kikemur.es/tatucircus', 7),
-(22, 'Pasacalles por la convivencia en Delicias', 'Desfile y actividades para fomentar la integración y diversidad.', 'Calles de Delicias', '2025-05-09 17:00', '2025-05-09 19:00', 'http://zaragoconecta.es/pasacalles', 1),
-(23, 'Fiesta del Sol', 'Celebración al aire libre con música y talleres de concienciación ecológica.', 'Parque Emilio Lacambra', '2025-05-10 11:00', '2025-05-10 00:00', 'http://zaragoconecta.es/fiestadelsol', 5), 
-(24, 'Mundialito Antirracista de Zaragoza 2025', 'Torneo de fútbol contra la discriminación.', 'CDM La Granja', '2025-05-17 09:00', '2025-05-17 19:00', 'http://zaragoconecta.es/mundialito', 1),
-(25, '25º aniversario CSA La Revuelta', 'Jornada con debates, conciertos y exposiciones.', 'CSA La Revuelta', '2025-09-13 12:00', '2025-09-13 23:00', 'http://revuelta.es/25aniversario', 1);
+(19, 'Concierto solidario con la PAH', 'Evento musical para recaudar fondos.', 'CSP Amapola', '2025-03-29 19:00', '2025-03-29 21:00', 'https://i.pinimg.com/736x/ae/84/50/ae8450303fc703320a0c5691d88d381e.jpg', 6),
+(20, 'Concierto Hillbilly Moon Explosion', 'Desde Suiza, The Hillbilly Moon Explosion es una de las bandas europeas de referencia Rockabilly.', 'Arrebato', '2025-03-30 11:00', '2025-03-30 18:00', 'https://i.pinimg.com/736x/8a/08/26/8a0826ee6626197006a4ba847b8c9d37.jpg', 1),
+(21, 'Tatu Circus Zgz', 'Festival de tatuajes solidario con eventos culturales.', 'CSO Kike Mur', '2025-05-02 10:00', '2025-05-04 22:00', 'https://zgz.convoca.la/media/0c3d76509349689944ae2778e10d9c8d.jpg', 7),
+(22, 'Concierto Benéfico', 'Concierto benéfico para las victimas de la dana', 'Calles de Delicias', '2025-05-09 17:00', '2025-05-09 19:00', 'https://imagenes.heraldo.es/files/image_1920_1080/uploads/imagenes/2024/04/13/cocadictos-con-alma-como-voz-cantante-en-plena-actuacion-durante-la-muestra-de-pop-rock-y-otros-rollos-zaragoza-1984.jpeg', 6),
+(23, 'Fiesta del Sol', 'Celebración al aire libre con música y talleres de concienciación ecológica.', 'Parque Emilio Lacambra', '2025-05-10 11:00', '2025-05-10 00:00', 'https://media.istockphoto.com/id/1971980189/es/vector/ilustraci%C3%B3n-vectorial-de-estilo-maravilloso-que-camina-por-el-sol-con-botas-y-muestra-el.jpg?s=612x612&w=0&k=20&c=DFyxdrq485aOr2lKltS1sYQKWSpXynelzMyE0AFRESU=', 5), 
+(24, 'Concierto benéfico castores', 'Recaudación de madera para las casas de los castores', 'CDM La Granja', '2025-05-17 09:00', '2025-05-17 19:00', 'https://i.pinimg.com/736x/13/2b/86/132b86a85bbbfe656d30deb9a3e8f0c5.jpg', 1),
+(25, 'Jornada solidaria por los osos polares', 'el dinero destinado será para cubitos de hielo', 'CSA La Revuelta', '2025-09-13 12:00', '2025-09-13 23:00', 'https://www.zaragozarebelde.org/wp-content/uploads/2009/04/f_00318.jpg', 1);
 SET IDENTITY_INSERT Eventos OFF;
 
 --La conversión del tipo de datos varchar en datetime produjo un valor fuera de intervalo.
